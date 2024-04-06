14356ef05c48d903f5dc444b81f9ae2651d43e9b
joaovitorss <42008605+joaovitorss@users.noreply.github.com>
2024-04-06 10:01:57 -0300

    Add files via upload

---
+++ desafio_area_dados.js
@@ -0,0 +1,10 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da Área de Circuferência é: "
++ areaCirc + "m².")
+
